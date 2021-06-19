4115ca9afd0440f814cea9bde540aa0abe535313
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2021-06-19 14:45:52 +0200

    init

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../models/question';
 import { QuestionService } from '../services/question.service';
 
 @Component({
@@ -8,24 +9,91 @@ import { QuestionService } from '../services/question.service';
 })
 export class QuestionComponent implements OnInit {
 
-  decile: number = 0;
+  question: Question = new Question({ "id": 2, "question": " POUR LA BANQUE MONDIALE- LE SEUIL INTERNATIONAL DE PAUVRET\u00c9 EXTR\u00caME EST FIX\u00c9  \u00c0", "choices_count": 3, "choices_content": ": A 1-25 dollar par jour ### B 1-90 dollar par jour ### C 5 dollars par  jour", "answer": "B", "karma": -23 });
+  questionStack: Question[] = [];
+  isAnswerAllGood: boolean = true;
+  nextId: number = 0;
+
+  questionTextView: string = '';
+  tipTextView: string = '';
+  answerATextView: string = '';
+  answerBTextView: string = '';
+  answerCTextView: string = '';
+  answerDTextView: string = '';
+  answerETextView: string = '';
+  answerFTextView: string = '';
+
+  getdecile: number = 0;
+  post: string = "";
+  getfirst: string = "";
+  getquestion: string = "";
 
   constructor(private serQuestion: QuestionService) { }
 
+
   ngOnInit(): void {
     this.getDecile();
+    this.getFirst();
+    this.getQuestion();
   }
-
-  getDecile(): void {
-    console.log('start')
-    this.serQuestion.getDecile('14').subscribe((data: any) => {
+  
+  getFirst(): void {
+    console.log('first')
+    this.serQuestion.subGetFirstQuestionJSON().subscribe((data: any) => {
       console.log(data);
-      this.decile = data;
+      this.getfirst = data;
     });
   }
 
-  // getDecile(): void {
-  //   console.log('start')
-  //   this.decile = this.serQuestion.getDecile('14');
+  getQuestion(): void {
+    console.log('get')
+    this.serQuestion.subGetQuestionJSON('14').subscribe((data: any) => {
+      console.log(data);
+      this.getquestion =  JSON.stringify(data);
+    });
+  }
+
+
+  // post(): void {
+  //   console.log('post')
+  //   this.serQuestion.getQuestionJSON(this.getfirst).subscribe((data: any) => {
+  //     console.log(data);
+  //     this.post = data;
+  //   });
   // }
+
+  getDecile(): void {
+    console.log('decile')
+    this.serQuestion.getDecile('14').subscribe((data: any) => {
+      console.log(data);
+      this.getdecile = data;
+    });
+  }
+
+  declareReplaybtn(): void {
+    //5678
+  }
+
+  declareLinkbtn(): void {
+    //5678
+  }
+  declareAbtn(): void {
+    //5678
+  }
+  declareBbtn(): void {
+    //5678
+  }
+  declareCbtn(): void {
+    //5678
+  }
+  declareDbtn(): void {
+    //5678
+  }
+  declareEbtn(): void {
+    //5678
+  }
+  declareFbtn(): void {
+    //5678
+  }
+
 }
--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -9,20 +9,34 @@ import { Observable } from 'rxjs';
 })
 export class QuestionService {
 
+  private FIRST_URL_GET = "/api/v1/first/";
+  private URL_GET = "/api/v1/question/";
+  private URL_POST = "/api/v1/send/";
+  private DECILE_URL_GET = "/api/v1/decile/";
 
-  private DECILE_URL_GET = "http://129.213.40.35:5000/decile/";
   constructor(private http: HttpClient) { }
 
-  getDecile(lastId:string) {
-     return this.http.get(this.DECILE_URL_GET+lastId);
+  getQuestionJSON(json: string) {
+      this.http.post<any>(this.URL_POST, json).subscribe({
+        next: data => {
+          return data;
+        },
+        error: error => {
+          console.error('There was an error with URL_POST!', error);
+        }
+      })
   }
 
+  subGetFirstQuestionJSON() {
+    return this.http.get(this.FIRST_URL_GET);
+  }
 
+  subGetQuestionJSON(nextId: string) {
+    return this.http.get(this.URL_GET + nextId);
+  }
 
-  // getDecile(lastId: string): any {
-  //   return fetch(this.DECILE_URL_GET + lastId, {
-  //     mode: 'cors',
-  //     credentials: 'include'
-  //   })
-  // }
+  getDecile(lastId: string) {
+    return this.http.get(this.DECILE_URL_GET + lastId);
+  }
+
 }
